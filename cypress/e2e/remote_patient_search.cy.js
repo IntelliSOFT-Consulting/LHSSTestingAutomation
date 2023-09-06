06121fdb7d69d424655c8fa3d510f5100d3dd304
javan <ochiengjavan133@gmail.com>
2023-09-06 11:53:28 +0300

    Update on remote search

--- cypress/e2e/remote_patient_search.cy.js
+++ cypress/e2e/remote_patient_search.cy.js
@@ -2,7 +2,7 @@ import '../support/commands'
 //import './new_user_registration_spec.cy'
 describe("Acccess the LHSS platform",()=>{
     beforeEach(()=>{
-        cy.base_url()
+        cy.baseurl()
         cy.login() 
         cy.contains("Login").click()
         //cy.get('.ke-apptoolbar > .ke-toolbar-item > a').click()
@@ -10,29 +10,20 @@ describe("Acccess the LHSS platform",()=>{
 
 
       })
-     // it.only("test1-tests functionality of the name search bar in the search page",()=>{
-        
-       // cy.contains("Cross Border").click()
-
-       // cy.get(':nth-child(2) > .ke-menu-item > .ke-icon > div > img').click()
-
-        //cy.get('input[name="name-query"]').type(firstName)
-
-      // cy.get('input[name="cb-id-query"]').type('X-061982-M-160520-160114-101508')
-     // })
-      it.only("test2-tests functionality of the local search bar by ID in the CB Module",()=>{
+      it.only("test1-tests functionality of the remote search by tying a search to a name and gender ",()=>{
 
          cy.contains("Cross Border").click()
-         //cy.get(':nth-child(6) > .ng-pristine').type(patientID)
-         //cy.get('#local-results').click()
          cy.get('[type="radio"]').first().check().wait(500)
         
          cy.get('input[name="name-query"]').type("Nyanchama");
-         //cy.get('#search-patient').click()
+         cy.get('input[name="gender-query"]').first().check()
+         cy.get('input[name="select-mpi"]').check()
+
+         cy.get('select[name="mpi-query"]').focus().select("Cross border MPI")
+         cy.get('input[id="remote-results"]').check()
          cy.get('#search-patient').click()
 
       
       })
     })
 
-export default {firstName};
