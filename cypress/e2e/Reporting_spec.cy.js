9de1d16d9e5ec518d076f673b8a12c48655432a9
javan <ochiengjavan133@gmail.com>
2023-09-07 13:21:21 +0300

    Updated cb referral

--- cypress/e2e/Reporting_spec.cy.js
+++ cypress/e2e/Reporting_spec.cy.js
@@ -15,6 +15,17 @@ describe("Acccess the homepage menu of the platform",()=>{
     cy.contains("Cross Border Referral Indicator").click().wait(5000)
     cy.contains("View").click()
 
+    })
+    it.only("test2-tests for requestinf for a CB summary Report",()=>{
+    cy.contains("Reports").click()
+    cy.contains("Cross Border Patient Summary").click().wait(5000)
+    cy.contains("View").click()
+
+    })
+     it.only("test1-tests querying functionality of the reports module",()=>{
+    cy.contains("Reports").click()
+    cy.contains("Cross Border Indicator report").click().wait(5000)
+    cy.contains("View").click()
 
     })
 
